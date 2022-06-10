387a00a42416bc615ec501a53e5552e6f7fffaef
bluehyro <bluehyro@gmail.com>
2022-06-10 20:37:56 +0300

    reversed-strings codewars challenge

---
+++ reversed-strings.js
@@ -0,0 +1,7 @@
+// split atskiria argumente nurodyto string raides ar zodzius ir pavercia i array
+// reverse apsuka array i kita puse
+// join sujungia array elementus i viena
+
+function solution(str){
+    return str.split('').reverse().join('');
+  }

04436d144a0e684d1bf142a10248738dfbe08680
bluehyro <bluehyro@gmail.com>
2022-07-29 16:15:00 +0300

    Codewars keep-hydrated challenge

---
+++ keep-hydrated.js
@@ -0,0 +1,7 @@
+// Buvo panaudota funkcija Math.floor(), kuri lengvai padėjo išspręsti užduotį, kadangi
+// Math.floor skaičių apvalina į mažąją pusę visada, todėl ši funkcija puikiai tiko užduočiai.
+
+
+function litres(time) {
+    return Math.floor(time*0.5)
+  }
